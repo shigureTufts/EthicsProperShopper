9295b8431295b120fd8ebf0170552ada5186b9b5
shigure <ju-hung.chen@tufts.edu>
2024-02-03 13:43:23 -0500

    build function in Q_learn, still figuring out how to implement q table

--- Q_Learning_agent.py
+++ Q_Learning_agent.py
@@ -23,7 +23,34 @@ class QLAgent:
         
     def learning(self, action, rwd, state, next_state):
         # implement the Q-learning function
+        state = self.trans(state)
+        next_state = self.trans(next_state)
+
+        if next_state not in self.qtable.index:
+            self.qtable = self.qtable.append(
+                pd.Series([0] * self.action_space, index=self.qtable.columns, name=next_state)
+            )
+
+        q_predict = self.qtable.loc[state, action]
+
+        if next_state not in self.qtable.index:
+            q_target = rwd
+        else:
+            q_target = rwd + self.gamma * self.qtable.loc[next_state, :].max()
+
+        self.qtable.loc[state, action] += self.alpha * (q_target - q_predict)
 
     def choose_action(self, state):
         # implement the action selection for the fully trained agent
-       
+        state = self.trans(state)
+
+        # Exploration-exploitation trade-off
+        if np.random.uniform(0, 1) < self.epsilon:
+            action = np.random.choice(self.action_space)
+        else:
+            action = self.qtable.loc[state, :].idxmax()
+
+        return action
+    def decay_epsilon(self):
+        if self.epsilon > self.mini_epsilon:
+            self.epsilon *= self.decay
